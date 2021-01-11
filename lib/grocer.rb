5f97198a660ab8a95afaa586882d509e7de9e663
calm-collection-3916 <khalinredd@gmail.com>
2021-01-11 01:57:39 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,20 +13,24 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  consolidated = {}
-  cart.each do |contents|
-    contents.each do |item, info|
-    if consolidated.include?(item)
-      consolidated[item][:count] += 1
-    else
-      consolidated[item] = {
-          :price => info[:price],
-          :clearance => info[:clearance],
-          :count => 1
-      }
-    end
-  end
-end
-consolidated
-end
+  index = 0
+  new_cart = []
   
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item
+      new_cart_index = 0
+      new_cart.each do |new_cart_item|
+        if new_cart_item[:item] === current_item[:item]
+          new_cart_item[:count] += 1
+        end
+        new_cart_index += 1
+      end
+    else
+      grocery_item[:count] = 1
+      new_cart << grocery_item
+    end
+    index += 1
+  end
+  new_cart
+end 
